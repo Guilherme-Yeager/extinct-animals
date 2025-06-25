a3749c82c9aaddc12b8f75af37fe1e34e088eef2
Verenilson da Silva Souza <profeta.garoto@gmail.com>
2025-06-25 12:17:43 -0300

    splash feita com animacao

--- lib/ui/view/splash_view.dart
+++ lib/ui/view/splash_view.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:extinct_animals/ui/view/home_view.dart';
 import 'package:extinct_animals/ui/view_model/change_language_view_model.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/constants/colors_custom.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -9,27 +13,72 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
+// class _SplashViewState extends State<SplashView> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initConfig();
+//   }
+
+//   void _initConfig() async {
+//     final ChangeLanguageViewModel changeLanguageViewModel =
+//         ChangeLanguageViewModel.instance;
+//     await changeLanguageViewModel.modifyLanguageLabels();
+//     if (mounted) {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => HomeView()),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+
+
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    _initConfig();
-  }
 
-  void _initConfig() async {
-    final ChangeLanguageViewModel changeLanguageViewModel =
-        ChangeLanguageViewModel.instance;
-    await changeLanguageViewModel.modifyLanguageLabels();
-    if (mounted) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomeView()),
-      );
-    }
+    Timer(const Duration(seconds: 3), () async {
+      final ChangeLanguageViewModel changeLanguageViewModel =
+          ChangeLanguageViewModel.instance;
+      await changeLanguageViewModel.modifyLanguageLabels();
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeView()),
+        );
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final double widthScreen = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: ColorsCustom.main,
+      body: Center(
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: widthScreen * 0.1, end: widthScreen * 0.6),
+          duration: const Duration(seconds: 2),
+          curve: Curves.easeOut,
+          builder: (context, size, child) {
+            return SizedBox(
+              width: size,
+              child: Image.asset(
+                'assets/img/T-Rex.png',
+                //width: size,
+                fit: BoxFit.contain,
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
